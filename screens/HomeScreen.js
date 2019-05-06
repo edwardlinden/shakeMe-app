1c1a7483eb7518b4ffd02e403bfd0264501cd160
edwardlinden <edward.linden@live.se>
2019-05-06 11:59:02 +0200

    color

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -101,7 +101,7 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'white',
   },
   developmentModeText: {
     marginBottom: 20,
